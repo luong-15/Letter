2f4b50aa65a8d2d9f6b957ddb1cfea1eb0201884
luong-15 <127217015+luong-15@users.noreply.github.com>
2025-08-02 13:23:29 +0700

    fix be

--- server.js
+++ server.js
@@ -12,7 +12,8 @@ const port = process.env.PORT || 3000
 app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
-app.use(express.static(path.join(__dirname, "public")))
+/* Removed static file serving to let Vercel serve public folder automatically */
+// app.use(express.static(path.join(__dirname, "public")))
 
 // Google Sheets setup
 let sheets
